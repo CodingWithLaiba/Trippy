e0cc1d460d9911ca1a7d50608666fad9d7b5683b
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-17 22:12:43 +0500

    Implement About page with Navbar and Hero components

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,6 +1,11 @@
-
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import heroImg from "../assets/night.jpg";
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <>
+      <Navbar />
+      <Hero cName="w-full h-[70vh] relative" heroImg={heroImg} title="About" />
+    </>
+  );
 }
