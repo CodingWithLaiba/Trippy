6114b3617b4decc67954f29f436211fd333f5b9a
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-17 22:13:03 +0500

    Implement Home page with Navbar, Hero, and Destination components

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,23 @@
-
+import Hero from "../components/Hero"
+import Navbar from "../components/Navbar"
+import heroImage from '../assets/12.jpg'
+import Destination from "../components/Destination"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <Navbar />
+    <Hero 
+    cName="w-full h-screen relative"
+    heroImg={heroImage}
+    title = "Your Journey Your Story"
+    text = "Choose Your Favourit Destination"
+    btnText = "Travel Plan"
+    btnclass = "bg-white py-2.5 px-4 md:py-4 md:px-8 rounded-md md:text-lg text-sm font-bold text-black"
+    url= "/"
+    />
+    <Destination/>
+    </>
+    
   )
 }
