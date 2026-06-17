446e6cd3eb158ce4fef5dc1919079cc06f6a3bf2
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-17 22:10:21 +0500

    Refactor App component to include routing and remove Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import './App.css'
-import Navbar from './components/Navbar'
-// import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
+import About from "./routes/About";
 function App() {
- 
-
   return (
-<>
-<Navbar/>
-</>
-    
-  )
+    <>   
+      
+       <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path ="/about" element={<About/>}/>
+        <Route path ="/service" element={<Service/>}/>
+        <Route path ="/contact" element={<Contact/>}/>
+       </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
