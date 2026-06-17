1290517f4816680d601e6ebc2dc352c6d7aafa4f
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-17 22:12:53 +0500

    Implement Service page with Navbar and Hero components

--- src/routes/Service.jsx
+++ src/routes/Service.jsx
@@ -1,6 +1,12 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import heroImg from "../assets/night.jpg";
 
 export default function Service() {
   return (
-    <div>Service</div>
+    <>
+    <Navbar/>
+    <Hero cName="w-full h-[70vh] relative" heroImg={heroImg} title="Services" />
+    </>
   )
 }
