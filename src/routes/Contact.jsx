c2170f4ac096dcf7bbd9c9261df061812f821dff
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-18 20:45:27 +0500

    Add Footer component to Contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar";
 import heroImage from '../assets/2.jpg'
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 
 export default function Contact() {
   return (
     <>
     <Navbar/>
     <Hero cName="w-full h-[70vh] relative" heroImg={heroImage} title="Contact" />
+    <Footer/>
     </>
   )
 }
