9fbdb727b683fa794dc1f5c6343e3f7bc582415e
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-18 21:36:19 +0500

    Add ContactFoam component to Contact page

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -2,12 +2,14 @@ import Navbar from "../components/Navbar";
 import heroImage from '../assets/2.jpg'
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import ContactFoam from "../components/ContactFoam";
 
 export default function Contact() {
   return (
     <>
     <Navbar/>
     <Hero cName="w-full h-[70vh] relative" heroImg={heroImage} title="Contact" />
+    <ContactFoam/>
     <Footer/>
     </>
   )
