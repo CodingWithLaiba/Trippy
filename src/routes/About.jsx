ca1e48740b5b25c3bf0d388cbac078cfd34f90a6
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-18 20:45:38 +0500

    Add AboutUs component and Footer to About page

--- src/routes/About.jsx
+++ src/routes/About.jsx
@@ -1,11 +1,15 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import heroImg from "../assets/night.jpg";
+import Footer from "../components/Footer";
+import AboutUs from "../components/AboutUs";
 export default function About() {
   return (
     <>
       <Navbar />
       <Hero cName="w-full h-[70vh] relative" heroImg={heroImg} title="About" />
+      <AboutUs/>
+      <Footer/>
     </>
   );
 }
