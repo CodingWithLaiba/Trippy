d741b2ed88880c91aade30fba9617d3942759a25
Laiba Ijaz <laibaijaz904@gmail.com>
2026-06-18 20:44:02 +0500

    Add Trip and Footer components to Service page

--- src/routes/Service.jsx
+++ src/routes/Service.jsx
@@ -1,12 +1,16 @@
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import heroImg from "../assets/night.jpg";
+import Footer from "../components/Footer";
+import Trip from "../components/Trip";
 
 export default function Service() {
   return (
     <>
     <Navbar/>
     <Hero cName="w-full h-[70vh] relative" heroImg={heroImg} title="Services" />
+    <Trip/>
+    <Footer/>
     </>
   )
 }
